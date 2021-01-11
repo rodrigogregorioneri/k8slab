3b3241c45946e61b2953d81f5a1df99157bf9229
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2021-01-11 15:42:54 -0300

    subindo projeto inicial

---
+++ src/main/java/com/neriidev/k8slab/kubelab/KubelabApplication.java
@@ -0,0 +1,13 @@
+package com.neriidev.k8slab.kubelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubelabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neriidev/k8slab/kubelab/KubelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neriidev.k8slab.kubelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
